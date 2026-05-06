49109469239d32ba8e2dbc0984a6d7a8d6bd092a
milechy <hkobayashi@mooores.com>
2026-05-06 16:43:23 +0900

    Show run-all failure taxonomy in Telegram policy

--- src/telegram/policy-reader.js
+++ src/telegram/policy-reader.js
@@ -1,5 +1,6 @@
 const { COMMAND_ALLOWLIST } = require('../ralph/command-allowlist');
 const { TELEGRAM_RUN_ALL_ENV, telegramRunAllEnabled } = require('./execution-adapter');
+const { RUN_ALL_FAILURE_REASON_TAXONOMY } = require('./handlers');
 
 function listExecutionCommands() {
   return COMMAND_ALLOWLIST.map((entry) => ({
@@ -35,6 +36,31 @@ function executionPolicyStatus(env = process.env) {
       'command_allowlist_match',
       'shell_execution_policy_pass',
       `${TELEGRAM_RUN_ALL_ENV}=true`
+    ],
+    run_all_failure_reason_taxonomy: RUN_ALL_FAILURE_REASON_TAXONOMY,
+    run_all_failure_reason_normalization: {
+      plan_file_not_found: 'plan_file_missing',
+      command_not_allowed: 'command_not_allowlisted'
+    },
+    run_all_failure_audit_summary_fields: [
+      'ok',
+      'reason',
+      'reason_taxonomy',
+      'stage',
+      'executor',
+      'command',
+      'exit_code',
+      'started_at',
+      'finished_at',
+      'duration_ms',
+      'run_all_enabled',
+      'wired_to_runtime',
+      'execution_connected',
+      'commands_executed',
+      'files_modified',
+      'approval_id',
+      'plan_path',
+      'log_path'
     ],
     allowed_commands: listExecutionCommands(),
     unsupported_from_telegram: [
