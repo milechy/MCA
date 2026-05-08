b5dac6751ad9fcae6af2ef7826df3c8555436697
milechy <hkobayashi@mooores.com>
2026-05-08 18:29:38 +0900

    Apply runtime env preflight to Telegram config loading

--- src/telegram/config.js
+++ src/telegram/config.js
@@ -1,3 +1,6 @@
+const { SECRET_SCOPES } = require('../ralph/secrets-policy');
+const { verifyRuntimeEnvInjection } = require('../ralph/runtime-env-preflight');
+
 function parseCsvNumberList(value) {
   if (!value) return [];
   return String(value)
@@ -9,11 +12,20 @@ function parseCsvNumberList(value) {
 }
 
 function loadTelegramConfig(env = process.env) {
+  const runtime_env_preflight = verifyRuntimeEnvInjection({
+    target_env: 'local',
+    scope: SECRET_SCOPES.LOCAL_ONLY,
+    action: 'telegram_config_load',
+    required_env_keys: ['TELEGRAM_BOT_TOKEN'],
+    env
+  });
+
   return {
-    bot_token: env.TELEGRAM_BOT_TOKEN || null,
+    bot_token: runtime_env_preflight.ok ? env.TELEGRAM_BOT_TOKEN || null : null,
     allowed_user_ids: parseCsvNumberList(env.TELEGRAM_ALLOWED_USER_IDS),
     allowed_chat_ids: parseCsvNumberList(env.TELEGRAM_ALLOWED_CHAT_IDS),
-    dry_run: env.TELEGRAM_DRY_RUN !== 'false'
+    dry_run: env.TELEGRAM_DRY_RUN !== 'false',
+    runtime_env_preflight
   };
 }
 
