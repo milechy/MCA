69405e02f80017ca5a7957854e4d5a6f86626f28
milechy <hkobayashi@mooores.com>
2026-05-08 16:16:34 +0900

    Add explicit Ralph control decision and escalation types

--- src/ralph/types.js
+++ src/ralph/types.js
@@ -12,6 +12,7 @@ const PHASES = Object.freeze({
   DONE: 'DONE',
   PAUSED: 'PAUSED',
   BLOCKED: 'BLOCKED',
+  ESCALATED: 'ESCALATED',
   STOPPED_SECURITY: 'STOPPED_SECURITY'
 });
 
@@ -44,9 +45,22 @@ const CONTROL_ACTIONS = Object.freeze({
   ESCALATE: 'escalate'
 });
 
+const CONTROL_REASONS = Object.freeze({
+  RISK_5_SECURITY_STOP: 'risk_5_security_stop',
+  DIFF_APPROVAL_REQUIRED: 'diff_approval_required',
+  PRODUCTION_RISK_REQUIRES_PLAN_APPROVAL: 'production_risk_requires_plan_approval',
+  APPROVAL_MODE_RISK_THRESHOLD: 'approval_mode_risk_threshold',
+  FULLAUTO_HIGH_RISK_THRESHOLD: 'fullauto_high_risk_threshold',
+  MODE_CHANGE_REQUIRES_PLAN_APPROVAL: 'mode_change_requires_plan_approval',
+  RESUME_AFTER_SECURITY_STOP_REQUIRES_PLAN_APPROVAL: 'resume_after_security_stop_requires_plan_approval',
+  AUTO_EXECUTE_ALLOWED: 'risk_within_auto_execute_policy',
+  HUMAN_ESCALATION_REQUIRED: 'human_escalation_required'
+});
+
 module.exports = {
   PHASES,
   APPROVAL_TYPES,
   APPROVAL_STATUSES,
-  CONTROL_ACTIONS
+  CONTROL_ACTIONS,
+  CONTROL_REASONS
 };
